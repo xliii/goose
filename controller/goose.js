10848e6a64751d4c933286337a3383032670b4b4
XLIII <xliii@mail.com>
2017-05-14 23:32:03 +0300

    Back button

--- controller/goose.js
+++ controller/goose.js
@@ -68,7 +68,7 @@ router.delete('/:id', function(req, res) {
     })
 });
 
-router.get('/read/:id', function(req, res) {
+router.get('/:id', function(req, res) {
     Goose.findOne(req.params.id, function(goose) {
         if (goose) {
             res.render('goose', {
